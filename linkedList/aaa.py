04dc0d15c038b22d7fb62168625fa392d044ba28
ThiagoAlbqq <thiago.ce.al@gmail.com>
2025-05-20 09:43:18 -0300

    Removendo linkedList do tracking

--- linkedList/aaa.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Ler o arquivo Excel
-df = pd.read_excel('teste.xlsx')
-
-# Salvar como CSV corretamente
-df.to_csv('dados_formatados.csv', index=False, sep=',', encoding='utf-8')
--- linkedList/capitulo_3/q1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    printf("Quando potencia = 0, a função f(2) é executada, realizando operações de comparação (>= e ==).\n"
-           "Quando potencia = 1, a função f(3) é chamada, envolvendo operações de comparação (>=, ==, <).\n"
-           "Para valores de potencia > 1, a função f(n) é definida como n + 2.");
-}
