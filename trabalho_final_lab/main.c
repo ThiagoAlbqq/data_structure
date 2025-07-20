c816be0522f9a88055634b6f990fd0158f86a25a
ThiagoAlbqq <thiago.ce.al@gmail.com>
2025-07-19 23:36:40 -0300

    modularizando o menu

---
+++ trabalho_final_lab/main.c
@@ -0,0 +1,34 @@
+#include "menu.h"
+#include <stdio.h>
+
+int main() {
+  int choice = 0;
+  do {
+    // Chamada da função
+    show_menu("TESTANDO");
+    scanf("%d", &choice);
+    while (getchar() != '\n')
+      ;
+    switch (choice) {
+    case 1:
+      // Função de Inserir
+      break;
+    case 2:
+      // Função de Buscar
+      break;
+    case 3:
+      // Função de Atualizar
+      break;
+    case 4:
+      // Função de Excluir
+      break;
+    case 0:
+      printf("Saindo do programa...\n");
+      break;
+    default:
+      printf("Opção inválida! Tente novamente.\n");
+      break;
+    }
+  } while (choice != 0);
+  return 0;
+}
